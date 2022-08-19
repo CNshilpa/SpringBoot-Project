1e98cfa9c96bd6800c6f3673fd46948a1d5a38de
CNshilpa <shilpa.cnv@gmail.com>
2022-08-20 01:54:09 +0530

    Spring Boot Project

---
+++ src/main/java/com/example/demo/Airtel.java
@@ -0,0 +1,22 @@
+package com.example.demo;
+
+public class Airtel implements Sim {
+
+	public Airtel() {
+		System.out.println("airtel constructor called");
+	}
+
+	@Override
+	public void calling() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void data() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
